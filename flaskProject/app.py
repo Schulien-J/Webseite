b50c8e5a4d9a51d513867edb76fb248ee8a13712
Schulien-J <jj.konrad.de@gmail.com>
2025-02-08 16:02:07 +0100

    hi

--- flaskProject/app.py
+++ flaskProject/app.py
@@ -2,45 +2,43 @@
 from flask import Flask, send_file, request
 import numpy as np
 import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 from scipy.ndimage import gaussian_filter
 from flask_cors import CORS
 import io
 
 app = Flask(__name__)
 CORS(app)
-def generate_image(A,B,C,fx,fy,gx,gy,hx,hy,noise):
-    x_res, y_res = 2000, 1000
+def generate_image(A, B, C, fx, fy, gx, gy, hx, hy, noise):
+    x_res, y_res = 200, 100
     x = np.linspace(0, 5 * np.pi, x_res)
-    y = np.linspace(0, 2 * np.pi, y_res)
+    y = np.linspace(0, 5 * np.pi, y_res)
     X, Y = np.meshgrid(x, y)
-
-
-
 
     Z = (A * np.sin(fx * X) * np.cos(fy * Y) +
          B * np.sin(gx * X**2 + gy * Y**2) +
          C * np.sin(hx * X**3 + hy * Y**3))
 
-
     random_noise = np.random.randn(*Z.shape) * noise
     random_noise = gaussian_filter(random_noise, sigma=4)
     Z += random_noise
 
-    fig = plt.figure(figsize=(12, 6))
-    ax = fig.add_subplot(111, projection='3d')
-    surf = ax.plot_surface(X, Y, Z, cmap='viridis', edgecolors='none', linewidth=0, antialiased=False, shade=True)
+    fig = go.Figure(data=[go.Surface(z=Z, x=X, y=Y, colorscale='viridis')])
+    fig.update_layout(
+        scene=dict(
+            xaxis=dict(range=[0, 5 * np.pi]),
+            yaxis=dict(range=[0, 5 * np.pi]),
+            zaxis=dict(showgrid=False, showticklabels=False)
+        ),
+        scene_camera=dict(eye=dict(x=-1, y=-1, z=A+B+C))
 
-    ax.set_xticks([])
-    ax.set_yticks([])
-    ax.set_zticks([])
-    ax.set_box_aspect([4, 4, 0.8])
-    ax.grid(False)
+    )
 
+
+# Speichern als statisches Bild
     img_io = io.BytesIO()
-    plt.savefig(img_io, format='webp', bbox_inches='tight', dpi=250)
+    fig.write_image(img_io, format='png', scale=2)
     img_io.seek(0)
-    plt.close(fig)
-
     return img_io
 
 
@@ -55,7 +53,7 @@ def generate_img():
     gy = float(request.args.get('gy', 0.3))
     hx = float(request.args.get('hx', 0.01))
     hy = float(request.args.get('hy', 0.01))
-    noise = float(request.args.get('noise', 0))
+    noise = float(request.args.get('noise', 6))
     return send_file(generate_image(A,B,C,fx,fy,gx,gy,hx,hy,noise), mimetype='image/webp')
 
 
